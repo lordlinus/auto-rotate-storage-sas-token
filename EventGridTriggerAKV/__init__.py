f6e06c4bac2e0e240c942ee45cd510d1c1d638c1
ssattiraju <ssattiraju@microsoft.com>
2021-09-10 15:43:08 +0800

    Initial commit

--- EventGridTriggerAKV/__init__.py
+++ EventGridTriggerAKV/__init__.py
@@ -1,60 +1,45 @@
 import json
 import logging
+import os
 
 import azure.functions as func
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.keys import KeyClient
-from datetime import datetime, timedelta
-from azure.storage.blob import BlobServiceClient, generate_account_sas, ResourceTypes, AccountSasPermissions
-from azure.mgmt.storage import StorageManagementClient
-
-# https://docs.microsoft.com/en-us/rest/api/storageservices/create-account-sas
-
-subscription_id = '7c1d967f-37f1-4047-bef7-05af9aa80fe2'
-
-def get_storage_keys(group_name, storage_account_name, credential=DefaultAzureCredential()):
-    storage_client = StorageManagementClient(credential, subscription_id)
-    storage_keys = storage_client.storage_accounts.list_keys(group_name, storage_account_name)
-    storage_keys = {v.key_name: v.value for v in storage_keys.keys}
-    print('\tKey 1: {}'.format(storage_keys['key1']))
-    print('\tKey 2: {}'.format(storage_keys['key2']))
-    return storage_keys['key1'], storage_keys['key2']
-
-def get_storage_account_sas(storage_account_name, storage_account_key, permission, expiry=datetime.utcnow() + timedelta(hours=1)):
-    sas_token = generate_account_sas(
-        storage_account_name,
-        storage_account_key,
-        resource_types=ResourceTypes(object=True),
-        permission=AccountSasPermissions(read=True),
-        expiry=expiry)
-    logging.info(f'\tSAS Token: {sas_token}')
-    return sas_token
+from TriggerKeyRotation import run
 
 def main(event: func.EventGridEvent):
-    event_json = json.loads(event.get_json())
-    result = json.dumps({
-        'id': event.id,
-        'data': event_json,
-        'topic': event.topic,
-        'subject': event.subject,
-        'event_type': event.event_type,
-    })
-    logging.info('Python EventGrid trigger processed an event: %s', result)
+    event_json = event.get_json()
+    result = json.dumps(
+        {
+            "id": event.id,
+            "data": event_json,
+            "topic": event.topic,
+            "subject": event.subject,
+            "event_type": event.event_type,
+        }
+    )
+    logging.info("Python EventGrid trigger processed an event: %s", result)
 
-    credential = DefaultAzureCredential()
-    storage_account = event_json['ObjectName'].split('-')[0]
-    storage_container = event_json['ObjectName'].split('-')[1]
-
-    key_client = KeyClient(vault_url="https://sattiraju-kv-01.vault.azure.net/", credential=credential)
-    # service = BlobServiceClient(account_url=f"https://{storage_account}.blob.core.windows.net/", credential=credential)
-    logging.info(f"EventGridTriggerAKV: {result}")
-    k1,k2 = get_storage_keys('sattiraju-rg', storage_account)
-    logging.info(f'\tKey 1: {k1}')
-    logging.info(f'\tKey 2: {k2}')
-    sas_token = get_storage_account_sas(storage_account, k1, 'r')
-    logging.info(f'\tSAS Token: {sas_token}')
-
-    key_client.set_key("storage_sas_key",sas_token)
-
-    # storage_secret_key = key_client.get_secrets(event_json['ObjectName'])
-    # logging.info(storage_secret_key.value)
+    rg = os.getenv("ResourceGroup")
+    keyvault_name = os.getenv("KeyVaultName")
+    subscription_id = os.getenv("SubscriptionId")
+    client_id = os.getenv("ClientId")
+    storage_account = event_json["ObjectName"].split("-")[0]
+    storage_container = event_json["ObjectName"].split("-")[1]
+    mesg = ''
+    if (
+        event.event_type == "Microsoft.KeyVault.SecretExpired"
+        or event.event_type == "Microsoft.KeyVault.SecretExpiresSoon"
+    ):
+        mesg = run(
+            resource_group=rg,
+            keyvault_name=keyvault_name,
+            storage_account=storage_account,
+            storage_container=storage_container,
+            subscription_id=subscription_id,
+        )
+    elif event.event_type == "Microsoft.KeyVault.SecretNewVersionCreated":
+        mesg = f"A new SAS version created:  {event.get_json()}"
+    else:
+        mesg = f"Event Type: {event.event_type} \
+                 Event_info: {event_json}"
+    logging.info(mesg)
+    return mesg
---
+++ TriggerKeyRotation/__init__.py
@@ -0,0 +1,112 @@
+import os
+import logging
+import requests
+
+import azure.functions as func
+from azure.identity import DefaultAzureCredential, ManagedIdentityCredential
+from azure.keyvault.secrets import SecretClient
+from datetime import datetime, timedelta
+from azure.storage.blob import (
+    BlobServiceClient,
+    generate_account_sas,
+    ResourceTypes,
+    AccountSasPermissions,
+)
+from azure.mgmt.storage import StorageManagementClient
+
+
+def get_storage_keys(
+    resource_group,
+    storage_account_name,
+    subscription_id,
+    credential=DefaultAzureCredential(),
+):
+    storage_client = StorageManagementClient(credential, subscription_id)
+    storage_keys = storage_client.storage_accounts.list_keys(
+        resource_group, storage_account_name
+    )
+    storage_keys = {v.key_name: v.value for v in storage_keys.keys}
+    # print("\tKey 1: {}".format(storage_keys["key1"]))
+    # print("\tKey 2: {}".format(storage_keys["key2"]))
+    return storage_keys["key1"], storage_keys["key2"]
+
+
+def get_storage_account_sas(
+    storage_account_name,
+    storage_account_key,
+    permission=None,
+    expiry=datetime.utcnow() + timedelta(hours=1),
+):
+    sas_token = generate_account_sas(
+        storage_account_name,
+        storage_account_key,
+        resource_types=ResourceTypes(object=True),
+        permission=AccountSasPermissions(read=True),
+        expiry=expiry,
+    )
+    # logging.info(f"\tSAS Token: {sas_token}")
+    return sas_token
+
+
+def test_sas_token(storage_account_name, sas_token=None):
+    test_file_url = f"https://{storage_account_name}.blob.core.windows.net/data/test.txt?{sas_token}"
+    r = requests.get(test_file_url)
+    return True if r.status_code == 200 else False
+
+
+def create_akv_key(keyvault_name, key_name, value, credential):
+    secret_client = SecretClient(
+        vault_url=f"https://{keyvault_name}.vault.azure.net/", credential=credential
+    )
+    secret = secret_client.set_secret(
+        key_name, value, expires_on=datetime.utcnow() + timedelta(hours=6)
+    )
+    return secret.properties.version, secret.properties.expires_on
+
+
+def run(
+    resource_group,
+    storage_account,
+    storage_container,
+    keyvault_name,
+    subscription_id=None,
+):
+    credentials = ManagedIdentityCredential()
+    k1, k2 = get_storage_keys(
+        storage_account_name=storage_account,
+        resource_group=resource_group,
+        subscription_id=subscription_id,
+    )
+    sas_token = get_storage_account_sas(
+        storage_account_name=storage_account, storage_account_key=k1
+    )
+    if test_sas_token(storage_account_name=storage_account, sas_token=sas_token):
+        version, expires_on = create_akv_key(
+            keyvault_name=keyvault_name,
+            key_name=f"{storage_account}-{storage_container}-saskey",
+            value=sas_token,
+            credential=credentials,
+        )
+        return func.HttpResponse(f"This HTTP triggered function executed successfully. \
+            A new SAS token is created and added to Azure Key Valut \
+            version: {version} and expires at UTC: {expires_on}", status_code=200)
+    else:
+        return func.HttpResponse("This HTTP triggered function executed failed.", status_code=500)
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Python HTTP trigger function processed a request.")
+
+    storage_account = req.params.get("storage_account")
+    storage_container = req.params.get("container")
+    rg = req.params.get("resource_group")
+    keyvault_name = req.params.get("keyvault_name")
+    subscription_id = os.getenv("SubscriptionId")
+
+    response = run(
+        resource_group=rg,
+        storage_account=storage_account,
+        storage_container=storage_container,
+        keyvault_name=keyvault_name,
+        subscription_id=subscription_id,
+    )
+    return response
